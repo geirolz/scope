10cea1e69cd3ab899052c20ebb52d5581fed04d3
Scala Steward <me@scala-steward.org>
2021-11-28 17:47:22 +0100

    Update cats-core to 2.7.0

--- project/ProjectDependencies.scala
+++ project/ProjectDependencies.scala
@@ -18,7 +18,7 @@ object ProjectDependencies {
 
   private val effects: Seq[ModuleID] = {
     Seq(
-      "org.typelevel" %% "cats-core" % "2.6.1"
+      "org.typelevel" %% "cats-core" % "2.7.0"
     )
   }
 
