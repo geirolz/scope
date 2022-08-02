0de1b83160997ef713dbc09be2aafaba4096a20e
Scala Steward <scala_steward@virtuslab.com>
2022-08-02 14:24:09 +0000

    Update scalactic to 3.2.13

--- project/ProjectDependencies.scala
+++ project/ProjectDependencies.scala
@@ -23,7 +23,7 @@ object ProjectDependencies {
   }
 
   private val tests: Seq[ModuleID] = Seq(
-    "org.scalactic" %% "scalactic" % "3.2.12",
+    "org.scalactic" %% "scalactic" % "3.2.13",
     "org.scalatest" %% "scalatest" % "3.2.12" % Test,
     "org.scalameta" %% "munit" % "0.7.29" % Test
   )
