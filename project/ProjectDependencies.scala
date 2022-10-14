4a8a23fb19dedb9fd6625aaf00d3ddc4f05b218d
Scala Steward <scala_steward@virtuslab.com>
2022-10-14 22:05:42 +0000

    Update scala-library, scala-reflect to 2.13.10

--- project/ProjectDependencies.scala
+++ project/ProjectDependencies.scala
@@ -25,7 +25,7 @@ object ProjectDependencies {
 
   object Generic {
     val scala2: Seq[ModuleID] = Seq(
-      "org.scala-lang" % "scala-reflect" % "2.13.8"
+      "org.scala-lang" % "scala-reflect" % "2.13.10"
     )
     val scala3: Seq[ModuleID] = Nil
   }
